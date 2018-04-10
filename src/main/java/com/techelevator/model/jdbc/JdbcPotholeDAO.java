b9fe3076ff0316d18b16ef92ca0c660c06993d03
Larsen Marquardt <Larsen.Marquardt@gmail.com>
2018-04-10 11:33:28 -0400

    JDBCPotholeDAO done

--- src/main/java/com/techelevator/model/jdbc/JdbcPotholeDAO.java
+++ src/main/java/com/techelevator/model/jdbc/JdbcPotholeDAO.java
@@ -1,4 +1,36 @@
 package com.techelevator.model.jdbc;
 
-public class JdbcPotholeDAO {
+import com.techelevator.model.Pothole;
+import com.techelevator.model.PotholeDAO;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class JdbcPotholeDAO implements PotholeDAO {
+
+    private JdbcTemplate jdbcTemplate;
+
+
+    @Override
+    public List<Pothole> getListOfPotholes() {
+
+
+        return null;
+    }
+
+
+    private Pothole mapRowToPothole(SqlRowSet results) {
+        Pothole thePothole = new Pothole();
+        thePothole.setStreetName(results.getString("street_name"));
+        thePothole.setStatusCode(results.getString("status_code"));
+        thePothole.setStatusDate(results.getDate("status_date").toLocalDate());
+        thePothole.setSeverity(results.getInt("severity"));
+        thePothole.setLongitude(results.getDouble("longitude"));
+        thePothole.setLatitude(results.getDouble("latitude"));
+        thePothole.setReportDate(results.getDate("report_date").toLocalDate());
+        return thePothole;
+    }
 }
