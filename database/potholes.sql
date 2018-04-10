fd93bfbbfb533a23eba4f3bf9eb3fab6ae4b4921
Larsen Marquardt <Larsen.Marquardt@gmail.com>
2018-04-10 10:23:11 -0400

    potholes database created

---
+++ database/potholes.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE potholes
+
+DROP TABLE pothole;
+
+CREATE TABLE pothole
+(
+    pothole_id SERIAL NOT NULL,
+    street_name varchar(200) NOT NULL,
+    status_code varchar(10) NOT NULL,
+    status_date timestamp NOT NULL,
+    severity int NOT NULL,
+    longitude decimal(3, 3) NOT NULL,
+    langitude decimal(3, 3) NOT NULL,
+    report_date timestamp NOT NULL,
+
+    CONSTRAINT pk_pothole_id PRIMARY KEY (pothole_id)
+);
