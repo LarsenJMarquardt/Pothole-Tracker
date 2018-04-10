ebfb26e1d77aaf5c92b77d0aea33f7adb1c82155
Larsen Marquardt <Larsen.Marquardt@gmail.com>
2018-04-10 10:35:13 -0400

    database slightly updated

--- database/potholes.sql
+++ database/potholes.sql
@@ -9,8 +9,8 @@ CREATE TABLE pothole
     status_code varchar(10) NOT NULL,
     status_date timestamp NOT NULL,
     severity int NOT NULL,
-    longitude decimal(3, 3) NOT NULL,
-    langitude decimal(3, 3) NOT NULL,
+    longitude decimal(3, 13) NOT NULL,
+    langitude decimal(3, 13) NOT NULL,
     report_date timestamp NOT NULL,
 
     CONSTRAINT pk_pothole_id PRIMARY KEY (pothole_id)
