05d95272ac4a42bccdd0e17420ab7e5e8464e6e7
Shana-Banana <shana.girard07@gmail.com>
2018-04-12 10:56:00 -0400

    added app_user to database

--- database/potholes.sql
+++ database/potholes.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS pothole;
+DROP TABLE IF EXISTS app_user;
 
 CREATE TABLE pothole
 (
@@ -14,13 +15,19 @@ CREATE TABLE pothole
     CONSTRAINT pk_pothole_id PRIMARY KEY (pothole_id)
 );
 
+CREATE TABLE app_user
+(
+  username varchar(32) NOT NULL,
+  password varchar(32) NOT NULL,
+  salt varchar(255),
+  is_employee boolean NOT NULL DEFAULT FALSE,
+  CONSTRAINT pk_app_user_username PRIMARY KEY (username)
+);
+
 INSERT INTO pothole (street_name, status_code, status_date, severity, longitude, latitude, report_date) VALUES ('Brian Rd', 'reported', '20080203', 4, 16.81712, 40.912132999999, '20171212');
 INSERT INTO pothole (street_name, status_code, status_date, severity, longitude, latitude, report_date) VALUES ('Katie Dr', 'inspected', '20080204', 3, 39.9928559, -83.0415075, '20170808');
 INSERT INTO pothole (street_name, status_code, status_date, severity, longitude, latitude, report_date) VALUES ('John Blvd', 'repaired', '20080205', 2, 123.1235891345, 534.9999999999999, '20170809');
 INSERT INTO pothole (street_name, status_code, status_date, severity, longitude, latitude, report_date) VALUES ('Steve Ave', 'reported', '20080206', 1, 51.9937101, 27.0398601, '20170810');
 INSERT INTO pothole (street_name, status_code, status_date, severity, longitude, latitude, report_date) VALUES ('Ben Ct', 'reported', '20080203', 5, 873.1235867345, 456.9912345999999, '20170811');
 
-SELECT * FROM pothole ORDER BY street_name asc;
-SELECT * FROM pothole ORDER BY status_code DESC;
-SELECT * FROM pothole ORDER BY severity DESC;
-SELECT * FROM pothole ORDER BY report_date DESC;
+INSERT INTO app_user (username, password) VALUES ('Sally_User', 'sally');
