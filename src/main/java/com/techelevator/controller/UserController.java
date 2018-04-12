c7f52b7fe3111e5dc642da1a25c03f163047a66e
Brandon <DeWittInTheRoad@gmail.com>
2018-04-12 11:28:29 -0400

    login built

--- src/main/java/com/techelevator/controller/UserController.java
+++ src/main/java/com/techelevator/controller/UserController.java
@@ -4,9 +4,12 @@ package com.techelevator.controller;
 import com.techelevator.model.UserDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
 
 
 @Controller
@@ -28,6 +31,11 @@ public class UserController {
 	public String createUser(@RequestParam String userName, @RequestParam String password) {
 		userDAO.saveUser(userName, password);
 		return "redirect:/login";
+	}
+
+	@RequestMapping(path="/user/{userName}", method=RequestMethod.GET)
+	public String displayDashboard(Map<String, Object> model, @PathVariable String userName) {
+		return "home";
 	}
 
 	
