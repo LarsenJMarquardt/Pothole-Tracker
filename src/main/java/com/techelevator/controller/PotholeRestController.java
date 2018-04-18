30cd11c69b400b522cc131657982ecbcb1337f1d
Larsen Marquardt <Larsen.Marquardt@gmail.com>
2018-04-18 11:57:33 -0400

    nothing really changed

--- src/main/java/com/techelevator/controller/PotholeRestController.java
+++ src/main/java/com/techelevator/controller/PotholeRestController.java
@@ -3,14 +3,10 @@ package com.techelevator.controller;
 import com.techelevator.model.Pothole;
 import com.techelevator.model.PotholeDAO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.annotation.RequestScope;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
